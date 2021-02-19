c7749bfab4fbb61fba4cb6bdd701672d644d3cbc
Rob Osborn <rosborn@webstacks.com>
2021-02-19 07:45:02 -0800

    changed stripe key to prod

--- utils/products.js
+++ utils/products.js
@@ -1,77 +1,77 @@
 //Prod DATA
-// const handleProd = (productId) => {
-//     switch (productId) {
-//         case "prod_IIH7RrPpa2OkUg": // PROD
-//             return {
-//                 name: "Authorify Digital Monthly Memberships",
-//                 prod: "Authorify"
-//             }
-//         case "prod_IIH7dwjQeJjrPo": // PROD
-//             return {
-//                 name: "Authorify Digital Annual Memberships",
-//                 prod: "Authorify"
-//             }
-//         case "prod_IIH7MrtHWAHGiS": // PROD
-//             return {
-//                 name: "Referral Marketing Annual",
-//                 prod: "RMA"
-//             }
-//         case "prod_IIH7f3l6dNhqqO": // PROD
-//             return {
-//                 name: "Referral Marketing Monthly",
-//                 prod: "RMA"
-//             }
-//         case "prod_IIH7BllxUwrBwA": // PROD
-//             return {
-//                 name: "Books on Demand",
-//                 prod: "DFY"
-//             }
-//         case "prod_IIH7szIpvaqH6R": // PROD
-//             return {
-//                 name: "DFY Seller Guides",
-//                 prod: "DFY"
-//             }
-//         case "prod_IIH7FzfC86pwZZ": // PROD
-//             return {
-//                 name: "DFY FSBO Guides",
-//                 prod: "DFY"
-//             }
-//         case "prod_IIH6CslJrLJ6sQ": // PROD
-//             return {
-//                 name: "DFY Expired Guides",
-//                 prod: "DFY"
-//             }
-//         case "prod_IHR6opYjiz60iV": // PROD
-//             return {
-//                 name: "Authorify Digital Membership Monthly",
-//                 prod: "Authorify"
-//             }
-//         case "57844425": // PROD
-//             return {
-//                 name: "Digital Trial",
-//                 prod: "Digital Trial"
-//             }
-//         case "prod_I56zgOlVWSndkq": // PROD
-//             return {
-//                 name: "Authorify Digital Membership Annual $997",
-//                 prod: "Authorify"
-//             }
-//         case "prod_I53duMFVHVWu8K": // PROD
-//             return {
-//                 name: "Authorify Digital+Plus Membership $97",
-//                 prod: "Authorify"
-//             }
-//         case "prod_FMcEiIFh8DOZD2": // PROD
-//             return {
-//                 name: "SmartAgents Labs - Gold",
-//                 prod: "SmartAgents Labs - Gold"
-//             }
-//         default:
-//             return {
-//                 error: "product not found in directory."
-//             }
-//     }
-// }
+const handleProd = (productId) => {
+    switch (productId) {
+        case "prod_IIH7RrPpa2OkUg": // PROD
+            return {
+                name: "Authorify Digital Monthly Memberships",
+                prod: "Authorify"
+            }
+        case "prod_IIH7dwjQeJjrPo": // PROD
+            return {
+                name: "Authorify Digital Annual Memberships",
+                prod: "Authorify"
+            }
+        case "prod_IIH7MrtHWAHGiS": // PROD
+            return {
+                name: "Referral Marketing Annual",
+                prod: "RMA"
+            }
+        case "prod_IIH7f3l6dNhqqO": // PROD
+            return {
+                name: "Referral Marketing Monthly",
+                prod: "RMA"
+            }
+        case "prod_IIH7BllxUwrBwA": // PROD
+            return {
+                name: "Books on Demand",
+                prod: "DFY"
+            }
+        case "prod_IIH7szIpvaqH6R": // PROD
+            return {
+                name: "DFY Seller Guides",
+                prod: "DFY"
+            }
+        case "prod_IIH7FzfC86pwZZ": // PROD
+            return {
+                name: "DFY FSBO Guides",
+                prod: "DFY"
+            }
+        case "prod_IIH6CslJrLJ6sQ": // PROD
+            return {
+                name: "DFY Expired Guides",
+                prod: "DFY"
+            }
+        case "prod_IHR6opYjiz60iV": // PROD
+            return {
+                name: "Authorify Digital Membership Monthly",
+                prod: "Authorify"
+            }
+        case "57844425": // PROD
+            return {
+                name: "Digital Trial",
+                prod: "Digital Trial"
+            }
+        case "prod_I56zgOlVWSndkq": // PROD
+            return {
+                name: "Authorify Digital Membership Annual $997",
+                prod: "Authorify"
+            }
+        case "prod_I53duMFVHVWu8K": // PROD
+            return {
+                name: "Authorify Digital+Plus Membership $97",
+                prod: "Authorify"
+            }
+        case "prod_FMcEiIFh8DOZD2": // PROD
+            return {
+                name: "SmartAgents Labs - Gold",
+                prod: "SmartAgents Labs - Gold"
+            }
+        default:
+            return {
+                error: "product not found in directory."
+            }
+    }
+}
 
 
 const handlePrice = (price) => {
@@ -523,78 +523,78 @@ const handlePrice = (price) => {
 
 
 // DEV DATA
-const handleProd = (productId) => {
-    switch (productId) {
-        case "prod_IIJDXvgHTLbldl": // DEV
-            return {
-                name: "Authorify Digital Monthly Memberships",
-                prod: "Authorify",
-            }
-        case "prod_IIIm7cRiUJ6yF9": // DEV
-            return {
-                name: "Authorify Digital Annual Memberships",
-                prod: "Authorify"
-            }
-        case "prod_III8H78OhIsleA": // DEV
-            return {
-                name: "Referral Marketing Annual",
-                prod: "RMA"
-            }
-        case "prod_IIIG84Qg04URPS": // DEV
-            return {
-                name: "Referral Marketing Monthly",
-                prod: "RMA"
-            }
-        case "prod_IIILrLjtnw1YEI": // DEV
-            return {
-                name: "Books on Demand",
-                prod: "DFY"
-            }
-        case "prod_IIIWGp1UHfBiyR": // DEV
-            return {
-                name: "DFY Seller Guides",
-                prod: "DFY"
-            }
-        case "prod_IIIb6LCYM30ZRm": // DEV
-            return {
-                name: "DFY FSBO Guides",
-                prod: "DFY"
-            }
-        case "prod_IIIflzf71sJJNs": // DEV
-            return {
-                name: "DFY Expired Guides",
-                prod: "DFY"
-            }
-        case "prod_IHR77yhDVvCdpW": // DEV
-            return {
-                name: "Authorify Digital Membership Monthly",
-                prod: "Authorify",
-            }
-        case "57844425": // DEV
-            return {
-                name: "Digital Trial",
-                prod: "Digital Trial"
-            }
-        case "prod_I56zgOlVWSndkq": // DEV
-            return {
-                name: "Authorify Digital Membership Annual $997",
-                prod: "Authorify"
-            }
-        case "prod_IH467IIZnDsQgM": // DEV
-            return {
-                name: "Authorify Digital+Plus Membership $97",
-                prod: "Authorify"
-            }
-        case "prod_FMcFUIKJl5e4xq": // DEV
-            return {
-                name: "SmartAgents Labs - Gold",
-                prod: "SmartAgents Labs - Gold"
-            }
-        default:
-            return {
-                error: "product not found in directory."
-            }
-    }
-}
+// const handleProd = (productId) => {
+//     switch (productId) {
+//         case "prod_IIJDXvgHTLbldl": // DEV
+//             return {
+//                 name: "Authorify Digital Monthly Memberships",
+//                 prod: "Authorify",
+//             }
+//         case "prod_IIIm7cRiUJ6yF9": // DEV
+//             return {
+//                 name: "Authorify Digital Annual Memberships",
+//                 prod: "Authorify"
+//             }
+//         case "prod_III8H78OhIsleA": // DEV
+//             return {
+//                 name: "Referral Marketing Annual",
+//                 prod: "RMA"
+//             }
+//         case "prod_IIIG84Qg04URPS": // DEV
+//             return {
+//                 name: "Referral Marketing Monthly",
+//                 prod: "RMA"
+//             }
+//         case "prod_IIILrLjtnw1YEI": // DEV
+//             return {
+//                 name: "Books on Demand",
+//                 prod: "DFY"
+//             }
+//         case "prod_IIIWGp1UHfBiyR": // DEV
+//             return {
+//                 name: "DFY Seller Guides",
+//                 prod: "DFY"
+//             }
+//         case "prod_IIIb6LCYM30ZRm": // DEV
+//             return {
+//                 name: "DFY FSBO Guides",
+//                 prod: "DFY"
+//             }
+//         case "prod_IIIflzf71sJJNs": // DEV
+//             return {
+//                 name: "DFY Expired Guides",
+//                 prod: "DFY"
+//             }
+//         case "prod_IHR77yhDVvCdpW": // DEV
+//             return {
+//                 name: "Authorify Digital Membership Monthly",
+//                 prod: "Authorify",
+//             }
+//         case "57844425": // DEV
+//             return {
+//                 name: "Digital Trial",
+//                 prod: "Digital Trial"
+//             }
+//         case "prod_I56zgOlVWSndkq": // DEV
+//             return {
+//                 name: "Authorify Digital Membership Annual $997",
+//                 prod: "Authorify"
+//             }
+//         case "prod_IH467IIZnDsQgM": // DEV
+//             return {
+//                 name: "Authorify Digital+Plus Membership $97",
+//                 prod: "Authorify"
+//             }
+//         case "prod_FMcFUIKJl5e4xq": // DEV
+//             return {
+//                 name: "SmartAgents Labs - Gold",
+//                 prod: "SmartAgents Labs - Gold"
+//             }
+//         default:
+//             return {
+//                 error: "product not found in directory."
+//             }
+//     }
+// }
 
 module.exports = { handleProd, handlePrice }
